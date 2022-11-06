577d99f935ff52af7c2933e422e7d7e49e04f6a9
moipav <moipav@ya.ru>
2022-11-06 20:49:57 +0800

    Initial commit

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,14 @@
+@extends('layout')
+
+@section('content')
+    <div class="contaner" align="center">
+    <h1 align="center">My galery</h1>
+        <div class="row">
+            <div class="col-md-3 p-3 border bg-light">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, architecto corporis doloribus eveniet ex exercitationem, expedita libero maxime, numquam placeat provident quis quisquam tenetur. Asperiores est impedit iure molestias nostrum.</div>
+            <div class="col-md-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, architecto corporis doloribus eveniet ex exercitationem, expedita libero maxime, numquam placeat provident quis quisquam tenetur. Asperiores est impedit iure molestias nostrum.</div>
+            <div class="col-md-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, architecto corporis doloribus eveniet ex exercitationem, expedita libero maxime, numquam placeat provident quis quisquam tenetur. Asperiores est impedit iure molestias nostrum.</div>
+            <div class="col-md-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, architecto corporis doloribus eveniet ex exercitationem, expedita libero maxime, numquam placeat provident quis quisquam tenetur. Asperiores est impedit iure molestias nostrum.</div>
+
+        </div>
+    </div>
+@endsection
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-light blue-200">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/hello">hello</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/about">about</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled">Disabled</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+@yield('content')
+
+</body>
+</html>
